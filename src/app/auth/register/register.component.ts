bbaf455a6eac71c59dd185955dcd2bc3dcfd8eb2
jhonathan55 <56006953+jhonathan55@users.noreply.github.com>
2021-10-29 19:20:32 -0300

    footer

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 //importa componetes a utilizar
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import Swal from 'sweetalert2';
 //importamos el AuthService
@@ -13,14 +13,37 @@ import { AuthService } from '../services/auth.service';
   providers: [AuthService]
 })
 export class RegisterComponent implements OnInit {
-  //crea metodo registerFrom que recibe dos parametros
-  registerForm = new FormGroup({
-    email: new FormControl(''),
-    password: new FormControl('')
 
-  })
+    //declaramos variables para validar form
+    private isValidEmail = /\S+@\S+\.\S+/;
+    private lengthPassword = 6;
+    private lengthCell = 9;
+    private number= "/^-?[0-9][^\.]*$/"; 
+
+  //crea metodo registerFrom que recibe dos parametros
+ 
+
+  registerForm = this.fb.group({
+    //Validators.pattern metodo de FormBuelder qlue valida con una expresion regular previamente creado
+    name: ['', [Validators.required]],
+    email: ['', [Validators.required, Validators.pattern(this.isValidEmail)]],
+    cel: ['', [Validators.required, 
+      Validators.maxLength(this.lengthCell),
+      Validators.minLength(this.lengthCell)
+      
+    ]
+      ],
+    //minLength valida la cantidad min de caracteres
+    password: ['', [Validators.required, Validators.minLength(this.lengthPassword)]],
+    politicas:['',[Validators.requiredTrue]]
+  });
+ 
   //declaramos una variable desde AuthService
-  constructor(private authSvc: AuthService, private router:Router) { }
+  constructor(
+    private authSvc: AuthService, 
+    private router:Router,
+    private fb: FormBuilder
+    ) { }
 
   ngOnInit(): void {
   }
@@ -36,6 +59,11 @@ export class RegisterComponent implements OnInit {
       console.error(error);
       
     }
+  }
+   //metodo valida campos vacios y muestra el error en txtbox con la propiedad de boostrap is-valid
+   isValidField(field: string): string {
+    const validatedField = this.registerForm.get(field);
+    return (!validatedField?.valid && validatedField?.touched) ? 'is-invalid' : validatedField?.touched ? 'is-valid' : '';
   }
 
 }
